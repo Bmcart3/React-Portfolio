d717c89a819f892b5ebcc69031d403d4222a6072
bmcart3 <bmcart3@gmail.com>
2020-07-07 09:56:12 -0400

    updated contacts page and added pdf of resume

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,7 +6,10 @@ const Contact = () => (
   <Container text>
     <div className="test">
       <Header as="h2">Contact Information</Header>
-      <p>Contact information with links to LinkedIn and GitHub will go here.</p>
+      <p><a href="../../images/BenCardWebDev.pdf" target="_blank" style={{color:"black"}} >Resume</a></p>
+      <p><a href="https://github.com/Bmcart3" target="_blank" style={{color:"black"}}>GitHub</a></p>
+      <p><a href="https://www.linkedin.com/in/ben-cart" target="_blank" style={{color:"black"}}>LinkedIn</a></p>
+      
     </div>
   </Container>
 );
