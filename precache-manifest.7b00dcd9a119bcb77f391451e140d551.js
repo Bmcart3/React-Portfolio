44bb264369ed49ce84331f01f0edf9233b1273d7
bmcart3 <bmcart3@gmail.com>
2020-07-08 17:04:44 -0400

    Updates

---
+++ precache-manifest.7b00dcd9a119bcb77f391451e140d551.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3662da448b38bda1b8405d767099f10c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a6e02bbf969280bdf15d",
+    "url": "/react-portfolio/static/css/main.684e0ff7.chunk.css"
+  },
+  {
+    "revision": "8464b8266975b92a5a2a",
+    "url": "/react-portfolio/static/js/2.b8e50fae.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/react-portfolio/static/js/2.b8e50fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e02bbf969280bdf15d",
+    "url": "/react-portfolio/static/js/main.5a98d9cc.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "34fcc832855567dddfa17dd2a3821a77",
+    "url": "/react-portfolio/static/media/SomethingDifferent.34fcc832.png"
+  },
+  {
+    "revision": "c2d0629e6b99173f725201ade7cb7c80",
+    "url": "/react-portfolio/static/media/WeatherDashboard.c2d0629e.PNG"
+  },
+  {
+    "revision": "f546052f4e0442958b69b04bb827c386",
+    "url": "/react-portfolio/static/media/dayPlanner.f546052f.PNG"
+  },
+  {
+    "revision": "5617459c112b6fd17b02ab36ef0039e8",
+    "url": "/react-portfolio/static/media/profilepic.5617459c.jpg"
+  },
+  {
+    "revision": "3d787d9a711d0d95e2ad9eb50e7ad4b9",
+    "url": "/react-portfolio/static/media/project2.3d787d9a.PNG"
+  }
+]);
